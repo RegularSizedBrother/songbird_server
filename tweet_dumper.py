28303019590fa03e87aa9a95280ed530a3e6e90e
lesliesun016 <33109069+lesliesun016@users.noreply.github.com>
2020-02-06 16:44:06 -0500

    Remove byte from cvs file

--- tweet_dumper.py
+++ tweet_dumper.py
@@ -51,7 +51,7 @@ def get_all_tweets(screen_name):
 	outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
 	
 	#write the csv	
-	with open('%s_tweets.csv' % screen_name, 'w') as f:
+	with open('%s_tweets.csv' % screen_name, 'wb') as f:
 		writer = csv.writer(f)
 		writer.writerow(["id", "created_at", "text"])
 		writer.writerows(outtweets)
