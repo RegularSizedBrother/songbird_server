904098686be960f9087d9c8eef731a6359d5de5a
Elaina Liu <elainaliu15@gmail.com>
2020-02-08 19:00:00 -0500

    Update test_twitter_personality.py
    Changed relative path to fix error

--- test/twitter_ibm_tests/test_twitter_personality.py
+++ test/twitter_ibm_tests/test_twitter_personality.py
@@ -1,5 +1,9 @@
 from songbird_server.resources.tweet_dumper import TwitterDumper
 from songbird_server.resources.personality_insights_twitter import TwitterPersonality
+import os
+
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+empty_file = os.path.join(curr_dir, 'empty_tweets.csv')
 
 def test_more_than_cap_twitter():
     user = "BarackObama"
@@ -22,7 +26,7 @@ def test_less_than_cap_twitter():
     assert profile is not None and len(profile) > 5
 
 def test_empty_twitter():
-    file_result = "empty_tweets.csv"
+    file_result = empty_file
     person_call = TwitterPersonality()
     profile = person_call.traits_to_vector(person_call.get_profile(file_result))
     print(profile)
