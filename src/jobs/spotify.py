d27da1b0e7d24eb9c99b4c726a54c5e40c540667
ceryshughes <hughes.1647@osu.edu>
2020-04-07 00:37:32 -0400

    Revert "Cleaned up changes"
    This reverts commit 660369dae9a5dc946712b401e4e713e8f6ef150a.

--- src/jobs/spotify.py
+++ src/jobs/spotify.py
@@ -24,9 +24,11 @@ def process_spotify(id):
         }
 
         genres = tweet_genres.get_genres_from_profile(attributes)
+        print(genres[0])
         playlist = spotify.generate_playlist(recommendation.handle, genres)
+        genres = ", ".join(genres[0])
         recommendation.playlist = playlist
-        recommendation.genres = ", ".join(genres[0])[0:255]
+        recommendation.genres = genres
 
         db.session.commit()
 
