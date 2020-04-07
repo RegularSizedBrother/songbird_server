8508ec464d1edf4911cfd55da3c1d753e7389cf7
ceryshughes <hughes.1647@osu.edu>
2020-04-07 01:00:13 -0400

    Tiny change to clean up

--- src/jobs/spotify.py
+++ src/jobs/spotify.py
@@ -25,9 +25,9 @@ def process_spotify(id):
 
         genres = tweet_genres.get_genres_from_profile(attributes)
         playlist = spotify.generate_playlist(recommendation.handle, genres)
-        genres = ", ".join(genres[0])
         recommendation.playlist = playlist
-        recommendation.genres = genres
+        recommendation.genres = ", ".join(genres[0])
+        print(recommendation.genres)
 
         db.session.commit()
 
