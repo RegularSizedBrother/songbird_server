cd73a19f6a65a152a936b7fa8f7f5694ac719367
Elaina Liu <elainaliu15@gmail.com>
2020-04-21 17:06:13 -0400

    Update twit_reply.py

--- src/resources/twit_reply.py
+++ src/resources/twit_reply.py
@@ -1,17 +1,14 @@
-from src.resources.config import create_api
 from src.models.recommendation import Recommendation, db
 
 import tweepy
 
 class TwitterReply():
-    def reply_with_spotify(self, api, tweet):
-        username = tweet.user.screen_name
-        records = Recommendation.query.filter_by(handle=username).all()
+    def reply_with_spotify(self, api, user):
+        records = Recommendation.query.filter_by(handle=user).all()
         record = records[-1]
         spotify_url = record.playlist
 
         api.update_status(
-            status="Hello! We made a playlist based on your Twitter activity. Listen now: {spotify_url}",
-            in_reply_to_status_id=tweet.id,
+            "@{user} Hello! We made a playlist based on your Twitter activity. Listen now: {spotify_url}"
         )
 
